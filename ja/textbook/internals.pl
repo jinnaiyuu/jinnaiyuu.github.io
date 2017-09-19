78cc2fdfe4ba42d69b087590244746135d8fb102
Yuu <ddyuudd@gmail.com>
2017-09-19 19:29:14 -0400

    put current textbook

---
+++ ja/textbook/internals.pl
@@ -0,0 +1,478 @@
+# LaTeX2HTML 2016 (1.71)
+# Associate internals original text with physical files.
+
+
+$key = q/cite_culberson1997sokoban/;
+$ref_files{$key} = "$dir".q|textbook.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_pearson1990/;
+$ref_files{$key} = "$dir".q|textbook.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/alg:init-zobrist-hashing/;
+$ref_files{$key} = "$dir".q|textbook.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:pddl-instance/;
+$ref_files{$key} = "$dir".q|textbook.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/tbl:search-problem-formulation/;
+$ref_files{$key} = "$dir".q|textbook.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_zobrist:70/;
+$ref_files{$key} = "$dir".q|textbook.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:symbolic-search/;
+$ref_files{$key} = "$dir".q|textbook.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/ch:heuristic-search/;
+$ref_files{$key} = "$dir".q|textbook.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:heursitic/;
+$ref_files{$key} = "$dir".q|textbook.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:grid-pathfinding/;
+$ref_files{$key} = "$dir".q|textbook.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_yap2002grid/;
+$ref_files{$key} = "$dir".q|textbook.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/alg:astar-search/;
+$ref_files{$key} = "$dir".q|textbook.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/alg:external-astar/;
+$ref_files{$key} = "$dir".q|textbook.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/ch:heuristic-serach-variants/;
+$ref_files{$key} = "$dir".q|textbook.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:coverage/;
+$ref_files{$key} = "$dir".q|textbook.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:greedy-best-first-search/;
+$ref_files{$key} = "$dir".q|textbook.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_lipovetzky2015a/;
+$ref_files{$key} = "$dir".q|textbook.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_erdem2005genome/;
+$ref_files{$key} = "$dir".q|textbook.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_held1970traveling/;
+$ref_files{$key} = "$dir".q|textbook.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/alg:expand/;
+$ref_files{$key} = "$dir".q|textbook.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:width-based-heuristic-search/;
+$ref_files{$key} = "$dir".q|textbook.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:parallel-overheads/;
+$ref_files{$key} = "$dir".q|textbook.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/ch:introduction/;
+$ref_files{$key} = "$dir".q|textbook.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_mcquillan1980new/;
+$ref_files{$key} = "$dir".q|textbook.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:msa-to-grid/;
+$ref_files{$key} = "$dir".q|textbook.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:automated-heuristic/;
+$ref_files{$key} = "$dir".q|textbook.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:15-puzzle/;
+$ref_files{$key} = "$dir".q|textbook.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_russelln03/;
+$ref_files{$key} = "$dir".q|textbook.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:binary-decision-diagram/;
+$ref_files{$key} = "$dir".q|textbook.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_algfoor2015comprehensive/;
+$ref_files{$key} = "$dir".q|textbook.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:astar-search/;
+$ref_files{$key} = "$dir".q|textbook.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_johnson1879notes/;
+$ref_files{$key} = "$dir".q|textbook.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_sturtevant2012benchmarks/;
+$ref_files{$key} = "$dir".q|textbook.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/tbl:open-list-data-structure/;
+$ref_files{$key} = "$dir".q|textbook.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/ch:blind-search/;
+$ref_files{$key} = "$dir".q|textbook.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_jinnai2016structured/;
+$ref_files{$key} = "$dir".q|textbook.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:iterative-width-search/;
+$ref_files{$key} = "$dir".q|textbook.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:novelty/;
+$ref_files{$key} = "$dir".q|textbook.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_jinnai2017work/;
+$ref_files{$key} = "$dir".q|textbook.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:state-space-problem/;
+$ref_files{$key} = "$dir".q|textbook.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_korf:85a/;
+$ref_files{$key} = "$dir".q|textbook.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:msa-solution/;
+$ref_files{$key} = "$dir".q|textbook.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:bidirectional-search/;
+$ref_files{$key} = "$dir".q|textbook.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_mcdowell2011cracking/;
+$ref_files{$key} = "$dir".q|textbook.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/alg:external-brfs/;
+$ref_files{$key} = "$dir".q|textbook.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:msa-cost/;
+$ref_files{$key} = "$dir".q|textbook.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:graph-search-algorithm/;
+$ref_files{$key} = "$dir".q|textbook.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:pddl/;
+$ref_files{$key} = "$dir".q|textbook.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/tbl:latency/;
+$ref_files{$key} = "$dir".q|textbook.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_aeronautiques1998pddl/;
+$ref_files{$key} = "$dir".q|textbook.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:width-based-search/;
+$ref_files{$key} = "$dir".q|textbook.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_pearl84/;
+$ref_files{$key} = "$dir".q|textbook.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/ch:search-performance/;
+$ref_files{$key} = "$dir".q|textbook.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:hash-distributed-astar/;
+$ref_files{$key} = "$dir".q|textbook.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/alg:wastar-open/;
+$ref_files{$key} = "$dir".q|textbook.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/alg:dfs-open/;
+$ref_files{$key} = "$dir".q|textbook.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_kishimotofb13/;
+$ref_files{$key} = "$dir".q|textbook.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_fikes:71/;
+$ref_files{$key} = "$dir".q|textbook.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_asai2014fully/;
+$ref_files{$key} = "$dir".q|textbook.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_edelkamp:2010:hst:1875144/;
+$ref_files{$key} = "$dir".q|textbook.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:breadth-first-search/;
+$ref_files{$key} = "$dir".q|textbook.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:why-search/;
+$ref_files{$key} = "$dir".q|textbook.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:search-problem-formulation/;
+$ref_files{$key} = "$dir".q|textbook.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:pddl-domain/;
+$ref_files{$key} = "$dir".q|textbook.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:sliding-token/;
+$ref_files{$key} = "$dir".q|textbook.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_sousa2013toward/;
+$ref_files{$key} = "$dir".q|textbook.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_nash2007theta/;
+$ref_files{$key} = "$dir".q|textbook.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:iterative-deepening-astar/;
+$ref_files{$key} = "$dir".q|textbook.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_helmert:08/;
+$ref_files{$key} = "$dir".q|textbook.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/alg:implicit-tree-search/;
+$ref_files{$key} = "$dir".q|textbook.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:external-brfs/;
+$ref_files{$key} = "$dir".q|textbook.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/tbl:basic-priority/;
+$ref_files{$key} = "$dir".q|textbook.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:novelty-heuristics/;
+$ref_files{$key} = "$dir".q|textbook.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:msa-to-grid-align/;
+$ref_files{$key} = "$dir".q|textbook.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/alg:iterative-deepening-astar/;
+$ref_files{$key} = "$dir".q|textbook.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_helmert2010scanalyzer/;
+$ref_files{$key} = "$dir".q|textbook.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:external-astar/;
+$ref_files{$key} = "$dir".q|textbook.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/ch:classical-planning/;
+$ref_files{$key} = "$dir".q|textbook.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/alg:bdd-brfs/;
+$ref_files{$key} = "$dir".q|textbook.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:heuristic-function/;
+$ref_files{$key} = "$dir".q|textbook.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/alg:improve/;
+$ref_files{$key} = "$dir".q|textbook.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_kautz:92/;
+$ref_files{$key} = "$dir".q|textbook.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/ch:state-space-problem/;
+$ref_files{$key} = "$dir".q|textbook.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:planning-definition/;
+$ref_files{$key} = "$dir".q|textbook.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/tbl:search-variants/;
+$ref_files{$key} = "$dir".q|textbook.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:heuristic-example/;
+$ref_files{$key} = "$dir".q|textbook.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/alg:cldfs/;
+$ref_files{$key} = "$dir".q|textbook.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:sokoban/;
+$ref_files{$key} = "$dir".q|textbook.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:depth-first-search/;
+$ref_files{$key} = "$dir".q|textbook.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:external-search/;
+$ref_files{$key} = "$dir".q|textbook.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:classical-planning-example/;
+$ref_files{$key} = "$dir".q|textbook.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:search-problem/;
+$ref_files{$key} = "$dir".q|textbook.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:weighted-astar-search/;
+$ref_files{$key} = "$dir".q|textbook.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:closed-list/;
+$ref_files{$key} = "$dir".q|textbook.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_jinnai2016automated/;
+$ref_files{$key} = "$dir".q|textbook.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:difficulity/;
+$ref_files{$key} = "$dir".q|textbook.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:novelty-based-pruning/;
+$ref_files{$key} = "$dir".q|textbook.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:tree-search-algorithm/;
+$ref_files{$key} = "$dir".q|textbook.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_burnslrz10/;
+$ref_files{$key} = "$dir".q|textbook.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/eq:abstract-zobrist/;
+$ref_files{$key} = "$dir".q|textbook.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:parallel-search/;
+$ref_files{$key} = "$dir".q|textbook.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/alg:bdd-brfs-tree/;
+$ref_files{$key} = "$dir".q|textbook.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_edgar2006multiple/;
+$ref_files{$key} = "$dir".q|textbook.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_applegate2006traveling/;
+$ref_files{$key} = "$dir".q|textbook.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/tbl:sliding-token/;
+$ref_files{$key} = "$dir".q|textbook.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_skiena2006programming/;
+$ref_files{$key} = "$dir".q|textbook.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_dijkstra1959note/;
+$ref_files{$key} = "$dir".q|textbook.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:branch-and-bound/;
+$ref_files{$key} = "$dir".q|textbook.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/alg:zobrist-hashing/;
+$ref_files{$key} = "$dir".q|textbook.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:symbolic-representation/;
+$ref_files{$key} = "$dir".q|textbook.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/eq:zobrist/;
+$ref_files{$key} = "$dir".q|textbook.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/alg:brfs-open/;
+$ref_files{$key} = "$dir".q|textbook.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/tbl:tree-vs-graph-search/;
+$ref_files{$key} = "$dir".q|textbook.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/alg:gfs-open/;
+$ref_files{$key} = "$dir".q|textbook.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_korf:2000/;
+$ref_files{$key} = "$dir".q|textbook.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/alg:implicit-graph-search/;
+$ref_files{$key} = "$dir".q|textbook.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:dijkstra/;
+$ref_files{$key} = "$dir".q|textbook.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:open-list/;
+$ref_files{$key} = "$dir".q|textbook.html|; 
+$noresave{$key} = "$nosave";
+
+1;
+
